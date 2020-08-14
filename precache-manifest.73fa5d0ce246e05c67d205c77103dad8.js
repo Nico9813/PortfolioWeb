06bed111f0d919a3f31f38c1679c53937d249409
nico9813 <nico.gomez.mbc@gmail.com>
2020-08-14 15:01:38 -0300

    Updates

---
+++ precache-manifest.73fa5d0ce246e05c67d205c77103dad8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c51a02742994c5e2dfd4ea38c230c31",
+    "url": "/PortfolioWeb/index.html"
+  },
+  {
+    "revision": "b4b2dab316f496d393f8",
+    "url": "/PortfolioWeb/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2f3334cda56c4d4d9e68",
+    "url": "/PortfolioWeb/static/css/main.368e2d4c.chunk.css"
+  },
+  {
+    "revision": "b4b2dab316f496d393f8",
+    "url": "/PortfolioWeb/static/js/2.0a10b042.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PortfolioWeb/static/js/2.0a10b042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3334cda56c4d4d9e68",
+    "url": "/PortfolioWeb/static/js/main.76746ed4.chunk.js"
+  },
+  {
+    "revision": "31dbcb143c80ff9a0ed3",
+    "url": "/PortfolioWeb/static/js/runtime-main.ae590e6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PortfolioWeb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "283e46bfcb6046af9665e2ded038dab7",
+    "url": "/PortfolioWeb/static/media/prueba.283e46bf.png"
+  }
+]);
