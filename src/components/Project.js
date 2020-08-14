99d775ce7882dc3e275e4d4c22b4c53316c04a6a
nico9813 <nico.gomez.mbc@gmail.com>
2020-08-14 15:25:28 -0300

    fix responsive images

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,13 +10,13 @@ export const Project = (props) => {
     return (
         <div>
             <Row className='projectRow' style={{ backgroundColor: left ? '#eff5ff' : '#c2d8fd' }}>
-                    <Col lg={{ size: 5, order: left,offset:1 }} className='projectCol'>
+                <Col xs={{size: 12}} lg={{ size: 5, order: left,offset:1 }} className='projectCol'>
                         <Container>
                             <h2>{proyectoActual.titulo}</h2>
                             <Row style={{marginLeft:1}}>
                                 {proyectoActual.tecnologias.map((item, index) =>
                                     <Col xs={{size:'auto'}} className="tecnologia" >
-                                        <h7>H: {item}</h7>
+                                        <h7>{item}</h7>
                                     </Col>
                                 )}
                             </Row>
@@ -24,7 +24,7 @@ export const Project = (props) => {
                             <Button onClick={() => window.location.href= proyectoActual.github } color="primary">View on Github</Button>
                         </Container>
                     </Col>
-                    <Col lg={{ size: 5, order: right }} className='projectCol'>
+                    <Col xs={{size: 12}} lg={{ size: 5, order: right }} className='projectCol'>
                         <Container>
                             <img src={proyectoActual.imagenes} style={{ maxWidth:'100%', maxHeight:'100%'}}/>
                         </Container>
