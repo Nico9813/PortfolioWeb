f2f6230317587c779efb5649761eff535bfdaf75
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-05 16:48:47 -0300

    color

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,19 +9,19 @@ export const Project = (props) => {
     const right = (props.contador + 1) % 2
 
     return (
-        <div className='projectRow' style={{ backgroundColor: left ? '#1f1f1f' : '#4287f5' }}>
+        <div className='projectRow' style={{ backgroundColor: left ? 'white' : '#7abfff' }}>
             <Row className='project'>
                 <Col xs={{size: 12}} lg={{ size: 3, order: left,offset:1 }} className='projectCol'>
                         <Container>
-                            <h2 style={{ color: left ? '#ebebeb' : 'black'}}>{proyectoActual.titulo}</h2>
+                            <h2>{proyectoActual.titulo}</h2>
                             <Row>
                                 {proyectoActual.tecnologias.map((item, index) =>
-                                    <Col xs={{ size: 'auto' }} className="tecnologia" style={{ backgroundColor: left ? '#4287f5' : '#1f1f1f' }}>
+                                    <Col xs={{ size: 'auto' }} className="tecnologia" style={{ backgroundColor: 'black' }}>
                                         <h7>{item}</h7>
                                     </Col>
                                 )}
                             </Row>
-                            <p style={{ color: left ? '#ebebeb' : 'black' }}>{proyectoActual.descripcion}</p>
+                            <p>{proyectoActual.descripcion}</p>
                             <Button onClick={() => window.location.href= proyectoActual.github } color="primary">View on Github</Button>
                         </Container>
                     </Col>
