8db4cb1df4e67575da07e7e9fc30c52351fd96c7
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-05 17:56:00 -0300

    diferent views

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CustomNavBar } from './components/NavBar'
 import { Header } from './components/Header'
 import { Project } from './components/Project'
@@ -8,16 +8,18 @@ import './App.css';
 
 import * as Perfil from './perfil.json'
 
+document.title = "Nicolas Gomez | Portfolio"
+
 function App() {
   return (
-    <div class="App">
-      <CustomNavBar foto={Perfil.foto}></CustomNavBar>
-      {Perfil.proyectos.map( (proyecto, index) => {
-        return(
-          <Project proyecto={proyecto} contador={index}></Project>
-        )
-      })}
-    </div>
+      <div class="App">
+        <CustomNavBar foto={Perfil.foto}></CustomNavBar>
+        {Perfil.proyectos.map((proyecto, index) => {
+          return (
+            <Project proyecto={proyecto} contador={index}></Project>
+          )
+        })}
+      </div>
   );
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -4,30 +4,35 @@ import Carrusel from './Carrusel'
 
 export const Project = (props) => {
 
-    const proyectoActual = props.proyecto
-    const left = props.contador % 2 
-    const right = (props.contador + 1) % 2
+    const { proyecto, contador } = props
+    const { titulo, descripcion, tecnologias, views } = proyecto
+    const left = contador % 2 
+    const right = (contador + 1) % 2
 
     return (
         <div className='projectRow' style={{ backgroundColor: left ? 'white' : '#7abfff' }}>
             <Row className='project'>
                 <Col xs={{size: 12}} lg={{ size: 3, order: left,offset:1 }} className='projectCol'>
                         <Container>
-                            <h2>{proyectoActual.titulo}</h2>
-                            <Row>
-                                {proyectoActual.tecnologias.map((item, index) =>
+                            <h2>{titulo}</h2>
+                            <Row style={{marginLeft:0.7}}>
+                                {tecnologias.map((item) =>
                                     <Col xs={{ size: 'auto' }} className="tecnologia" style={{ backgroundColor: 'black' }}>
                                         <h7>{item}</h7>
                                     </Col>
                                 )}
                             </Row>
-                            <p>{proyectoActual.descripcion}</p>
-                            <Button onClick={() => window.location.href= proyectoActual.github } color="primary">View on Github</Button>
+                            <p>{descripcion}</p>
+                            <Row style={{ marginLeft: 0.7 }}>
+                                {views.map((value) => 
+                                    <Button style={{ marginRight: "3px" }} onClick={() => window.location.href = value[Object.keys(value)]} color="primary">View on {Object.keys(value)}</Button>
+                                )}
+                            </Row>
                         </Container>
                     </Col>
                     <Col xs={{size: 12}} lg={{ size: 7, order: right }} className='projectCol'>
                         <Container>
-                            <Carrusel imagenes={proyectoActual.imagenes}/>
+                            <Carrusel imagenes={proyecto.imagenes}/>
                         </Container>
                     </Col>
             </Row>
