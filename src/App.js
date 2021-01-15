247a416511f81a0af505a58d3c9bf54c5757348f
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-15 16:56:11 -0300

    add portfolio image

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
             <Project proyecto={proyecto} contador={index}></Project>
           )
         })}
+        <Footer></Footer>
       </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Container } from 'reactstrap'
 
 export function Footer() {
     return (
         <div className="footer">
-            <Container>
-                
-            </Container>
         </div>
     )
 }
